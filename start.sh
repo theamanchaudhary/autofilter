7a5c6aede9188cf015e5a532754b8bf897a2f9f0
jarwisai <98968495+jarwisai@users.noreply.github.com>
2022-11-10 02:03:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /autofilter
 fi
-cd /AutoAnura
+cd /autofilter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
