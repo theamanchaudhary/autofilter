0882dc633f721a63c61de9aea0705ea2f36cdd69
jarwisai <98968495+jarwisai@users.noreply.github.com>
2022-11-10 02:06:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -378,27 +378,25 @@ def humanbytes(size):
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 async def get_shortlink(link):
-    print(worked)
-    return "omegalinks.in"
-#     https = link.split(":")[0]
-#     if "http" == https:
-#         https = "https"
-#         link = link.replace("http", https)
-#     url = f'https://omegalinks.in/api'
-#     params = {'api': URL_SHORTNER_WEBSITE_API,
-#               'url': link}
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://omegalinks.in/api'
+    params = {'api': URL_SHORTNER_WEBSITE_API,
+              'url': link}
 
-#     try:
-#         async with aiohttp.ClientSession() as session:
-#             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-#                 data = await response.json()
-#                 if data["status"] == "success":
-#                     return data['shortenedUrl']
-#                 else:
-#                     print(URL_SHORTNER_WEBSITE_API,URL_SHORTNER_WEBSITE_API,link)
-#                     logger.error(f"Error: {data['message']}")
-#                     return f'https://{URL_SHORTENR_WEBSITE}/st?api={URL_SHORTNER_WEBSITE_API}&url={link}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    print(URL_SHORTNER_WEBSITE_API,URL_SHORTNER_WEBSITE_API,link)
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{URL_SHORTENR_WEBSITE}/st?api={URL_SHORTNER_WEBSITE_API}&url={link}'
 
-#     except Exception as e:
-#         logger.error(e)
-#         return f'{URL_SHORTENR_WEBSITE}/st?api={URL_SHORTNER_WEBSITE_API}&url={link}'
+    except Exception as e:
+        logger.error(e)
+        return f'{URL_SHORTENR_WEBSITE}/st?api={URL_SHORTNER_WEBSITE_API}&url={link}'
