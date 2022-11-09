e36fc6a5f1053f9fbab1273ed4e360eeec780910
jarwisai <98968495+jarwisai@users.noreply.github.com>
2022-11-10 02:17:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,13 +13,13 @@ def is_enabled(value, default):
         return default
 
 class script(object):
-    HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://paisakamalo.in/')
+    HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://omegalinks.in/')
     START_TXT = """𝙷𝙴𝙻𝙾 Meri Jaan {},
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 From Movie Matin DB, 𝙹𝚄𝚂𝚃 Join My Group And Type And See My Power❤️❤️🔥"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}</b>
-<b>✮ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/AboutMdMatinAshraf>𝙼𝚍_𝙼𝚊𝚝𝚒𝚗</a></b>
+<b>✮ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/omegabots>omegabots</a></b>
 <b>✮ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</b>
 <b>✮ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹</b>
 <b>✮ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾-𝙳𝙱</b>
@@ -28,10 +28,10 @@ class script(object):
 <b>✮ 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻: <a href=https://youtube.com/channel/UC80IWL40N21zeY3pXriAOIg>𝕄𝕒𝕥𝕚𝕩_𝕋𝔾</a></b>"""
     SOURCE_TXT = """<b>NOTE:</b>
 - 𝕄𝕒𝕥𝕚𝕩 is a not open source project. 
-- Source - https://t.me/AboutMdMatinAshraf
+- Source - https://t.me/omegabots
 
 <b>DEVS:</b>
-- <a href=https://t.me/Movie_Matin>𝑀𝑂𝑉𝐼𝐸_𝑀𝐴𝑇𝐼𝑁</a>"""
+- <a href=https://t.me/omegabots>𝑀𝑂𝑉𝐼𝐸_𝑀𝐴𝑇𝐼𝑁</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and 𝕄𝕒𝕥𝕚𝕩 will respond whenever a keyword is found the message
@@ -56,7 +56,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/Movie_Matin)</code>
+<code>[Button Text](buttonurl:https://t.me/omegabots)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
