ed988539d5e34671f6971da8e7bf4ac9abc3139f
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2022-11-10 08:21:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/sources_cods/55"
+DOWNLOAD_TEXT_URL = "https://youtu.be/FAqTq5YZIoI"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Suscribe"
