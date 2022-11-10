f736fd26063ca622d7d09c3602a1014c002e603e
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2022-11-10 07:30:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/jarwisai/autofilter /autofilter
+  git clone https://github.com/Ashrafmdmatin41/autofilter /autofilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /autofilter
