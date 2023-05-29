f8bfa3c2caa3454ce7174c0d44bd37979b38f9bc
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-05-30 03:27:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://mplaylink.com/api'
+    url = f'https://oggylink.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link}
 
